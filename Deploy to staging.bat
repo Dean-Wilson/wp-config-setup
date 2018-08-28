28b402c36d4a0e8f51baa1166567ccb8a47499f1
Dean Wilson <d.a.wilson3@gmail.com>
2018-08-28 10:11:14 +1000

    allow public folder to be committed to repo and therefore stop expressly including it in deployment, add list mode for staging deployment too

--- Deploy to staging.bat
+++ Deploy to staging.bat
@@ -1,2 +1,27 @@
-@CALL phploy -s staging
+@ECHO OFF
+
+:: git branch
+:: git branch | find "* staging" > NUL & IF ERRORLEVEL 1 (
+::     
+::     ECHO.
+::     ECHO ---------------------------------------------
+::     ECHO  WARNING:
+::     ECHO  You are attempting to deploy to staging
+::     ECHO  but you are NOT on the staging branch.
+::     ECHO  Is this what you want to do?
+::     ECHO.
+::     ECHO  Hit CTRL+C now to cancel deployment.
+::     ECHO ---------------------------------------------
+::     ECHO.
+::     PAUSE
+::     
+:: )
+
+@CALL phploy -s staging --list
 @PAUSE
+
+git tag staging-deployment -f
+
+CALL phploy -s staging
+PAUSE
+EXIT
